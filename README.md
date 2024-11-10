78ea9586115c1ef9ff913e05c28cbda3caa2b50d
Shani Staretz <shakimi88@gmail.com>
2024-11-10 21:43:19 +0200

    lambda functions

--- README.md
+++ README.md
@@ -2,15 +2,39 @@
 
 ## subjects learned:
 * functions- continue:
-  * how to write documentation: after """""", click on 'enter' in the middle, if the function recieve paramter, the ide will add automatically:
+  * how to write documentation: after """""", click on 'enter' in the middle, if the function receive parameter, the ide will add automatically:
   for example:
- ```
-def my_headline(str1: str="") -> str:
-    """
-    :param str1: 
-    :return: 
-    """
-    ....
-  ```
-
+    ```
+    def my_headline(str1: str="") -> str:
+      """
+      :param str1: 
+      :return: 
+      """
+      ....
+    ```
+  * Scopes
+  * global parameters - it is not recommended to use it at all. but if used and need to change it, send it to the function as a parameter and return it back, updated.
+  * lambda functions - part of annonimus functions subject a way to short it to smaller function:
+    * must be 1 line
+    * must return something
+    * examples:
+      ```
+      def is_negative(number: int) -> bool:
+        return number < 0
+      is_negative_lambda=lambda  number:number<0
+      is_negative_lambda=lambda  number:True if number<0 else False
+      print(is_negative_lambda(-5)) # same as print(is_negative(-5))
+      ```
+      
+    * filter functions- (like in js) :a function that receive a lambda function and a list and return a filtered list by the condition in the lambda function
+      ```
+      l_str1 = ["apple", "Anaconda", "banana", "pineapple", "coconut", "Am-Pm"]
+      l6 = list(filter(lambda word: len(word) > 6, l_str1))
+      ```
+    * map function:( like in js)- a way to create a new value for each item in the received list and returns a new list with the new values.
+      ```
+       l1 = [-4, 0, 2, -1, 3, 9]
+      l_same=list(map(lambda x:x,l1))
+      ```
+      
 ## extra subjects:
--- main.py
+++ main.py
@@ -1,6 +1,68 @@
-def main():
-    pass
+counter = 10
 
+
+def inc_counter():
+    global counter
+    counter += 1
+
+
+def dec_counter():
+    global counter
+    counter -= 1
+
+
+def print_counter():
+    print("counter:", counter)
+
+
+def inc_local_counter():
+    counter: int = 20
+
+
+def is_negative(number: int) -> bool:
+    return number < 0
+
+
+is_negative_lambda = lambda number: number < 0
+power_2_lambda = lambda x: x ** 2
+is_positive = lambda x: x > 0
+
+
+def main():
+    # inc_counter()
+    # inc_counter()
+    # inc_counter()
+    # dec_counter()
+    # dec_counter()
+    # print_counter()
+    # inc_local_counter()
+    # print_counter()
+    # print(power_2_lambda(3))
+    # for x in [-4,0,2,-1,2,9]:
+    # results=[x  for x in [-4,0,2,-1,2,9] if is_positive (x)]
+    # print(results)
+    # l1=[-4,0,2,-1,3,9]
+    # l2=list(filter(lambda x:x!=0,l1))
+    # l3 = list(filter(lambda x: x %2==0, l1))
+    # l4 = list(filter(lambda x: x %2== 0 and x>0, l1))
+    # l5 = list(filter(lambda x: True == 0 and x > 0, l1))#all x in l1
+    # print("origin:", l1)
+    # print("not zero", l2)
+    # print("evens", l3)
+    # print("evens and positives", l4)
+    # l_str1 = ["apple", "Anaconda", "banana", "pineapple", "coconut", "Am-Pm"]
+    # l6 = list(filter(lambda word: len(word) > 6, l_str1))
+    # l7 = list(filter(lambda word: '-' in word, l_str1))
+    # print("strings with length>6:", l6)
+    # print("strings with the char '-' in them:", l7)
+    # l1 = [-4, 0, 2, -1, 3, 9]
+    # l_inc1= list(map(lambda item:item+1,l1))
+    # print(l_inc1)
+    l_str=["zzzzz","b","abc","cccc","bb","t"]
+    print(list(map(lambda word:len(word),l_str)))
+    l2 = [54, 20, 12, 133, 83, 9989]
+    print(list(map(lambda num:num%100//10,l2)))
+    print(list(map(lambda num:"even" if num%2==0 else "odd",l2 )))
 
 if __name__ == "__main__":
     main()
